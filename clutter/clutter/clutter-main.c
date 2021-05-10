4d97f6581adf8d609904dcea2fbfbb85b22d96f9
Carlos Garnacho <carlosg@gnome.org>
2021-05-10 19:57:37 +0200

    clutter: Export private function to get debug flags
    We're shuffling some code into src/backends, that needs knowing
    about these flags to minimize immediate refactors.

--- clutter/clutter/clutter-main.c
+++ clutter/clutter/clutter-main.c
@@ -2274,6 +2274,19 @@ clutter_remove_debug_flags (ClutterDebugFlag     debug_flags,
 }
 
 void
+clutter_get_debug_flags (ClutterDebugFlag     *debug_flags,
+                         ClutterDrawDebugFlag *draw_flags,
+                         ClutterPickDebugFlag *pick_flags)
+{
+  if (debug_flags)
+    *debug_flags = clutter_debug_flags;
+  if (draw_flags)
+    *draw_flags = clutter_paint_debug_flags;
+  if (pick_flags)
+    *pick_flags = clutter_pick_debug_flags;
+}
+
+void
 _clutter_set_sync_to_vblank (gboolean sync_to_vblank)
 {
   clutter_sync_to_vblank = !!sync_to_vblank;
